2fc426cd821fbcda968cf93a7a9d72e1975900e0
Yngvarr <k.yngvarr@gmail.com>
2023-08-29 00:04:48 +0200

    some additional analysis

--- src/pass/postprocess.clj
+++ src/pass/postprocess.clj
@@ -4,43 +4,64 @@
 (defn read-json [path]
   (json/parse-string (slurp path) true))
 
-;; FIX
-;; - links.target:
-;;     218960 -> 2189600
-;;     218857 -> 2188578
-;; - add links:
-;; {
-;;   "source": "https://itch.io/jam/day-1/rate/2187658",
-;;   "target": "https://itch.io/jam/day-2/rate/2188857"
-;; }, {
-;;   "source": "https://itch.io/jam/day-1/rate/2188145",
-;;   "target": "https://itch.io/jam/day-2/rate/2189179"
-;; }, {
-;;   "source": "https://itch.io/jam/day-2/rate/2189696",
-;;   "target": "https://itch.io/jam/day-3/rate/2191644"
-;; }, {
-;;   "source": "https://itch.io/jam/day-3/rate/2191734",
-;;   "target": "https://itch.io/jam/day-4/rate/2193386"
-;; }, {
-;;   "source": "https://itch.io/jam/day-2/rate/2190136",
-;;   "target": "https://itch.io/jam/day-3/rate/2191654"
-;; }, {
-;;   "source": "https://itch.io/jam/day-3/rate/2191529",
-;;   "target": "https://itch.io/jam/day-4/rate/2193378"
-;; }, {
-;;   "source": "https://itch.io/jam/day-3/rate/2191745",
-;;   "target": "https://itch.io/jam/day-4/rate/2193387"
-;; }, {
-;;   "source": "https://itch.io/jam/day-4/rate/2193375",
-;;   "target": "https://itch.io/jam/day-5/rate/2194411"
-;; }, {
-;;   "source": "https://itch.io/jam/day-3/rate/2191409",
-;;   "target": "https://itch.io/jam/day-4/rate/2192078"
-;; }, {
-;;   "source": "https://itch.io/jam/day-1/rate/2188275",
-;;   "target": "https://itch.io/jam/day-2/rate/2190253"
-;; }
+(defn find-game [graph entry-url]
+  (first (filter #(= entry-url (:entry-url %)) (:nodes graph))))
 
+(defn count-lonely [graph]
+  (count
+    (filter
+      (fn [entry]
+        (and (= 1 (:day entry))
+             (not-any? #(or
+                          (= (:entry-url entry)
+                             (:target %))
+                          (= (:entry-url entry)
+                             (:source %)))
+                       (:links graph))))
+      (:nodes graph))))
+
+(defn ancestor-descendant [graph {t :target s :source}]
+  (let [day-t (:day (find-game graph t))
+        day-s (:day (find-game graph s))]
+    (cond
+      (= day-t day-s) nil
+      (> day-t day-s) [s t]
+      :else [t s])))
+
+(defn median [arr]
+  (let [size (count arr)
+        sorted-arr (sort arr)]
+    (when (> size 0)
+      (if (odd? size)
+        (nth sorted-arr (/ size 2))
+        (/ (+ (nth sorted-arr (/ size 2))
+              (nth sorted-arr (dec (/ size 2))))
+           2)))))
 
 (comment
-  (read-json "docs/data.json"))
+  (def graph (read-json "docs/data.json"))
+  (def links (->> (:links graph)
+                  (mapv #(ancestor-descendant graph %))
+                  (filterv some?)))
+  (def links-by-day
+    (reduce
+      (fn [acc [a d]]
+        (let [day-a (dec (:day (find-game graph a)))]
+          (assoc-in
+            acc
+            [day-a a]
+            (let [res (get-in acc [day-a a])]
+              (if (nil? res) [d] (conj res d))))))
+      [{} {} {} {} {} {}] links))
+  (def descendants-num
+    (mapv #(reduce-kv
+             (fn [acc k v]
+               (conj acc (count v))) [] %)
+          links-by-day))
+  (mapv median descendants-num)
+
+  (sort (nth descendants-num 4))
+
+  (keys graph)
+  (find-game graph "https://itch.io/jam/day-6/rate/2195670")
+  (-> graph :nodes first))
